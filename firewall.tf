916fbc2b46a927df03b47edb33e3252f2acc0dc8
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2024-09-03 08:57:53 +0100

    init 4

--- firewall.tf
+++ firewall.tf
@@ -1,7 +1,8 @@
 # Define firewall rules for secure communication
+# network = google_compute_network.vpc_network.name
 resource "google_compute_firewall" "allow_internal" {
   name    = "allow-internal"
-  network = google_compute_network.vpc_network.name
+  network = "my-vpc"
 
   allow {
     protocol = "icmp"
