75c78f8d3ac9d2b7f0c2f95496e1e0c94910b56d
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2024-09-03 16:45:08 +0100

    init 8

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "google_compute_router" "router" {
 
 resource "google_compute_router_nat" "nat" {
   name                               = "my-router-nat"
-  router                             = google_compute_router.router[0].name
+  router                             = google_compute_router.router.name
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
