6bc34f1ff278577cbaa1a47223604b359617fd7a
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2024-09-04 10:03:49 +0100

    init 8

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    google= {
+      source ="hashicorp/google"
+    }
+    kubernetes={
+      source="hashicorp/kubernetes"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
