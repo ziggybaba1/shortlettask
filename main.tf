601e1ff95697678e629d232653456d8a8075938c
Ziggybaba1 <seyiadejugbagbe@gmail.com>
2024-09-04 11:05:37 +0100

    init 8

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "google_compute_subnetwork" "subnet" {
 
 
 provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
+  host                   = google_container_cluster.primary[0].endpoint
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 
